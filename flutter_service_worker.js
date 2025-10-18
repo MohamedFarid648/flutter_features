cedd2cfcac91965d5bb75a3b935fed1de171ee7a
Mohamed Abdullah_Eye <mohamed.abdallah@eye-ltd-eg.com>
2025-10-18 23:46:43 +0300

    ch

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b1ab68074d5f4a105f66730215751459
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4886e44fed4b307f12e88064dc11aead",
+"flutter_bootstrap.js": "a7e130f3b2c6c2cf5643375c909ea658",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
